9a1af3936581fb4b6b1785043b5e0e3a8217d60c
Serhun <sergoondenis@gmail.com>
2025-03-14 17:24:42 +0200

    changed the title

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Tap to Rule: Build Your Empire!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 10)}>
           Clicks {count}
